f3c967baf4f61531e37a8d5a06414cfd1bba4176
Darckfer <juliocesarcarrillorocha@gmail.com>
2024-10-23 18:02:30 +0200

    Rectangulo funcional

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rectangulo</title>
+</head>
+
+<body>
+    <form action="Rectangulo.php" method="post">
+        <input type="text" name="lado1" id="lado1">
+        <input type="text" name="lado2" id="lado2">
+        <input type="submit" value="Calcular">
+    </form>
+</body>
+
+</html>
